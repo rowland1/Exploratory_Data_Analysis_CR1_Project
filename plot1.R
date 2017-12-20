f56be921b0d4aa7d826971e9d6c71a3726e0bcdf
rowland1 <rowland1@nationwide.com>
2017-12-20 14:06:32 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,23 @@
+getwd()
+
+#read files and parse headers into columns#
+list.files()
+unzip("./exdata_data_household_power_consumption.zip")
+epc_data <- read.csv("./household_power_consumption.txt", header = T, sep = ';', na.strings="?", nrows = 2075259, check.names = F, stringsAsFactors = F, comment.char = "", quote = '\"')
+
+#format dates#
+epc_data$NewDate <- strptime(as.character(epc_data$Date), "%d/%m/%Y")
+epc_data$NewDate <- as.Date(epc_data$NewDate, format = "%d/%m/%Y")
+
+#subset data#
+epc_datasubset <- subset(epc_data, subset = (epc_data$NewDate >= "2007-02-01" & Date <= "2007-02-02"))
+
+#Plot 1#
+
+hist(epc_datasubset$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+
+#Save .png#
+
+dev.copy(png,file="Plot1.png", height=480, width=480)
+dev.off()
+                   
