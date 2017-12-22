896ea760d87877b32e1926be0c23b007a6b36c8f
rowland1 <rowland1@nationwide.com>
2017-12-22 11:47:22 -0500

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,35 @@
+getwd()
+
+#read files and parse headers into columns#
+list.files()
+unzip("./exdata_data_household_power_consumption.zip")
+epc_data <- read.csv("./household_power_consumption.txt", header = T, sep = ';', na.strings="?", nrows = 2075259, check.names = F, stringsAsFactors = F, comment.char = "", quote = '\"')
+
+#format dates#
+epc_data$NewDate <- strptime(as.character(epc_data$Date), "%d/%m/%Y")
+epc_data$NewDate <- as.Date(epc_data$NewDate, format = "%d/%m/%Y")
+epc_data$Datetime <- strptime(paste(epc_data$Date, epc_data$Time), "%d/%m/%Y %H:%M:%S")
+
+
+#subset data#
+epc_datasubset <- subset(epc_data, subset = (epc_data$NewDate >= "2007-02-01" & epc_data$NewDate <= "2007-02-02"))
+
+#Plot 4#
+
+par(mfrow = c(2,2))
+with(epc_datasubset, {
+    plot(epc_datasubset$Datetime, epc_datasubset$Global_active_power, type="l", ylab = "Global Active Power (kilowatts)", xlab="")
+    plot(epc_datasubset$Datetime, epc_datasubset$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
+    plot(epc_datasubset$Datetime, epc_datasubset$Sub_metering_1,type = "l", col = "black", xlab = "", ylab = "Energy Sub-Metering")
+      lines(epc_datasubset$Datetime, epc_datasubset$Sub_metering_1, col = "black")
+      lines(epc_datasubset$Datetime, epc_datasubset$Sub_metering_2, col = "red")
+      lines(epc_datasubset$Datetime, epc_datasubset$Sub_metering_3, col = "blue")
+      legend("topright", cex =0.5, bty = "n", col = c("black", "red", "blue"), c("Sub-Metering 1", "Sub-Metering 2", "Sub-Metering 3"), lwd = 1)
+      plot(epc_datasubset$Datetime, epc_datasubset$Global_reactive_power, type = "l", ylab = "Global Reactive Power", xlab = "datetime")
+})
+
+#Save .png#
+
+dev.copy(png,file="plot4.png", height=480, width=480)
+dev.off()
+                   
